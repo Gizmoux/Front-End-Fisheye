d9b9a3e02e8f57619963a3408ee1e7dcac4fe250
Nicolas <nicolas.deriemacker@hotmail.fr>
2023-12-08 16:02:25 +0100

    💄 Add Images et fetch datas

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,78 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+async function getPhotographers() {
+	// Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
+	// mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+	try {
+		// Utilisez await pour attendre que la promesse fetch soit résolue
+		const response = await fetch('../../data/photographers.json');
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+		// Vérifiez si la requête a réussi (statut HTTP 200)
+		if (!response.ok) {
+			throw new Error(`Erreur HTTP! Statut : ${response.status}`);
+		}
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+		// Convertissez la réponse en JSON
+		const data = await response.json();
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
-    
+		// Obtenez la liste des photographes depuis le fichier JSON
+		const photographers = data.photographers;
+
+		// Retournez les données récupérées
+		return {
+			photographers: photographers,
+		};
+	} catch (error) {
+		console.error('Erreur lors de la récupération des photographes :', error);
+		// return {
+		// 	photographers: [],
+		// };
+	}
+
+	// et bien retourner le tableau photographers seulement une fois récupéré
+	// return {
+	// 	photographers: [
+	// 		...photographers,
+	// 		...photographers,
+	// 		...photographers,
+	// 		...photographers,
+	// 	],
+	// };
+}
+
+async function displayData(photographers) {
+	const photographersSection = document.querySelector('.photographer_section');
+
+	photographers.forEach(photographer => {
+		const photographerModel = photographerTemplate(photographer);
+		const userCardDOM = photographerModel.getUserCardDOM();
+		photographersSection.appendChild(userCardDOM);
+	});
+}
+
+async function init() {
+	// Récupère les datas des photographes
+	const { photographers } = await getPhotographers();
+	displayData(photographers);
+}
+
+init();
+
+let photographers = [
+	{
+		name: 'Ma data test',
+		id: 1,
+		city: 'Paris',
+		country: 'France',
+		tagline: 'Ceci est ma data test',
+		price: 400,
+		portrait: 'account.png',
+	},
+	{
+		name: 'Autre data test',
+		id: 2,
+		city: 'Londres',
+		country: 'UK',
+		tagline: 'Ceci est ma data test 2',
+		price: 500,
+		portrait: 'account.png',
+	},
+];
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,28 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+	const { name, portrait, city, tagline, price, country } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+	const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+	function getUserCardDOM() {
+		const article = document.createElement('article');
+		const img = document.createElement('img');
+		const h2 = document.createElement('h2');
+		const h3 = document.createElement('h3');
+		const paragraphe = document.createElement('p');
+		const span = document.createElement('span');
+
+		img.setAttribute('src', picture);
+		h2.textContent = name;
+		(h3.textContent = city), country;
+		paragraphe.textContent = tagline;
+		span.textContent = `${price}${'€/jour'}`;
+
+		article.appendChild(img);
+		article.appendChild(h2);
+		article.appendChild(h3);
+		article.appendChild(paragraphe);
+		article.appendChild(span);
+		return article;
+	}
+	return { name, picture, getUserCardDOM };
 }
