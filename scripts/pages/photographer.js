38110341a93315aaacf2816699630fd1bccfb88e
Nicolas <nicolas.deriemacker@hotmail.fr>
2023-12-20 15:40:33 +0100

    💄 Add function createMedia and filterbutton

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,58 +1,30 @@
 // // Mettre le code JavaScript lié à la page photographer.html
 import { getPhotographers } from './index.js';
-// import { displayData } from '../pages/index.js';
 import { photographerTemplate } from '../templates/photographer.js';
 const urlParams = new URLSearchParams(window.location.search);
 const photographerIdURL = parseInt(urlParams.get('id'));
-console.log('photographerIdURL', photographerIdURL);
-console.log('urlParams', urlParams);
+
+// console.log('photographerIdURL', photographerIdURL);
+// console.log('urlParams', urlParams);
 
 const fetchDataPhotographer = async () => {
 	try {
 		// Utilisez getPhotographers pour récupérer les données
-		const { photographers, mediaPhotographers } = await getPhotographers();
-
+		const { photographers } = await getPhotographers();
 		// Trouver le photographe avec l'ID correspondant dans les données récupérées
 		const selectedPhotographer = photographers.find(
 			photographer => photographer.id === photographerIdURL
 		);
-		const selectedMediaPhotographer = mediaPhotographers
-			.filter(media => media.photographerId === photographerIdURL)
-			.map(element => {
-				return element.title;
-			});
-		console.log('selectedMediaPhotographer', selectedMediaPhotographer);
-
 		if (selectedPhotographer) {
 			const mainElement = document.querySelector('.photograph-header');
 			const divMediaElement = document.createElement('div');
 			const divMainElement = document.createElement('div');
-			divMediaElement.classList.add('photograph-media');
-			divMainElement.classList.add('photograph-presentation');
-
 			// Appeler la fonction qui affiche les détails du photographe
 			const { getUserCardDOM } = photographerTemplate(selectedPhotographer);
-
 			mainElement.appendChild(getUserCardDOM());
 			mainElement.appendChild(divMainElement);
-
-			selectedMediaPhotographer.forEach(media => {
-				const img = document.createElement('img');
-				const picture = `assets/images/${media.image}`;
-				console.log('PICTUREE', picture);
-				// console.log('media.photographerId', photographerId);
-				img.setAttribute('src', picture);
-				divMediaElement.appendChild(img);
-			});
-			selectedMediaPhotographer.forEach(title => {
-				const p = document.createElement('p');
-				p.textContent = title;
-				divMediaElement.appendChild(p);
-			});
-
-			// Ajouter divMediaElement au DOM
 			mainElement.appendChild(divMediaElement);
-			console.log('selectedMediaPhotographer2', selectedMediaPhotographer);
+
 			console.log('Détails du photographe sélectionné :', selectedPhotographer);
 		} else {
 			console.error("Photographe non trouvé avec l'ID spécifié.");
@@ -61,13 +33,62 @@ const fetchDataPhotographer = async () => {
 		console.log(error);
 	}
 };
-
 // Appeler la fonction pour récupérer et afficher les données du photographe
 fetchDataPhotographer();
 
 // Factory pour Media
-// const mediaFactory = () => {
-// 	selectedMediaPhotographer.map(elem => {
-// 		return elem.image;
-// 	});
-// };
+
+const mediaContainer = document.querySelector('.media-container');
+async function createMedia() {
+	const { mediaPhotographers } = await getPhotographers();
+	const selectedMediaPhotographer = mediaPhotographers
+		.filter(media => media.photographerId === photographerIdURL)
+		.forEach(media => {
+			const divCard = document.createElement('div');
+			divCard.classList.add('divCard');
+			const p = document.createElement('p');
+			p.textContent = media.title;
+
+			const likesElement = document.createElement('p');
+			likesElement.textContent = `Likes: ${media.likes}`;
+			if (media.image) {
+				const imageElement = document.createElement('img');
+				imageElement.src = `assets/images/${photographerIdURL}/${media.image}`;
+				imageElement.alt = media.title;
+				divCard.appendChild(imageElement);
+			} else if (media.video) {
+				const videoElement = document.createElement('video');
+				videoElement.src = `assets/images/${photographerIdURL}/${media.video}`;
+				videoElement.controls = true;
+				divCard.appendChild(videoElement);
+			}
+			mediaContainer.appendChild(divCard);
+			divCard.appendChild(p);
+			divCard.appendChild(likesElement);
+		});
+
+	// const filterOptions = document.getElementById('filter-button');
+
+	console.log('selectedMediaPhotographerTITLE', selectedMediaPhotographer);
+	console.log('mediaPhotographers', mediaPhotographers);
+}
+createMedia();
+
+// Bouton Filter
+const buttonFilter = () => {
+	const filterContainer = document.querySelector('#filter-container');
+	const select = document.createElement('select');
+	const filterOptions = ['Popularité', 'Date', 'Titre'];
+
+	console.log(select);
+
+	filterContainer.appendChild(select);
+
+	filterOptions.forEach(optionText => {
+		const option = document.createElement('option');
+		option.value = optionText;
+		option.text = optionText;
+		select.appendChild(option);
+	});
+};
+buttonFilter();
